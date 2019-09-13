a8787e4328c8c1ff3a62aa6313afc7fe9e8fc520
GSCrawley <gscrawley@gmail.com>
2019-09-12 19:35:26 -0700

    fixed and finished

--- checklist.py
+++ checklist.py
@@ -1,3 +1,4 @@
+print("Hello World")
 import sys
 
 def print(text):
@@ -22,37 +23,43 @@ def list_all_items():
        index += 1
 
 def mark_completed(index):
-   update(index, "√" + checklist[item])
+   update(index, "√" + checklist(index))
    list_all_items()
 
 def select(function_code):
+   #print("Please input a CRUD function: ")
    # Create item
-   if function_code == "C":
-       input_item = user_input("Input item:")
+   if function_code.upper() == "C":
+       input_item = crud_user_input("Input item:")
        create(input_item)
+       return True
    # Read item
-   elif function_code == "R":
-       item_index = user_input("Index Number")
+   elif function_code.upper() == "R":
+       item_index = crud_user_input("Index Number")
        # Remember that item_index must actually exist or our program will crash.
        print(read(int(item_index)))
-   elif function_code == "D":
-       index = user_input("Index Number")
-       destroy(index)
+       return True
+   elif function_code.upper() == "D":
+       index = crud_user_input("Index Number")
+       destroy(int(index))
        list_all_items()
-   elif function_code == "U":
-       index = user_input("Index Number")
-       item = user_input("item")
+       return True
+   elif function_code.upper() == "U":
+       index = crud_user_input("Index Number")
+       item = crud_user_input("item")
        update(int(index), item)
        list_all_items()
+       return True
    # Print all items
-   elif function_code == "P":
+   elif function_code.upper() == "P":
        list_all_items()
-   elif function_code == "Q":
+       return True
+   elif function_code.upper() == "Q":
        return False
-   # Catch all```
+   # Catch all
    else:
-       print("Unknown Option")
-   return True
+       print("Unknown Option \n")
+       return True
 
 # I did it a little differently - user is asked what dsy it is; console outputs Captain Rainbow's outfit according to that day.
 
@@ -60,12 +67,32 @@ def user_input():
    user_input = input("What day is it today?")
    return user_input
 
-colors = ["red","yellow","orange","blue","green","purple","pink"]
-clothing = ["shoes","socks","pants","shirt","jacket","tie","hat"]
+def crud_user_input(prompt):
+    crud_user_input = input(prompt)
+    return crud_user_input
 
 
-outfits = {
-"Sunday":colors[0]+" "+clothing[0]+
+# user_value = user_input("Please Enter a value:")
+# print(user_value)
+#
+# def test():
+#     create('Red Shoes')
+#     create('Blue Socks')
+#     create('Pink Shirt')
+#     print(read(0))
+#     print(read(1)) 
+#     print(read(2))   
+    
+
+
+
+if __name__ == "__main__":
+    colors = ["red","yellow","orange","blue","green","purple","pink"]
+    clothing = ["shoes","socks","pants","shirt","jacket","tie","hat"]
+
+
+    outfits = {
+    "Sunday":colors[0]+" "+clothing[0]+
         ", "+colors[1]+" "+clothing[1]+
         ", "+colors[2]+" "+clothing[2]+
         ", "+colors[3]+" "+clothing[3]+
@@ -115,31 +142,22 @@ outfits = {
             ", "+colors[4]+" "+clothing[5]+
             ", "+colors[5]+" "+clothing[6]
 
-}
-weekdays = list(outfits.keys())
+        }
+    weekdays = list(outfits.keys())
 
-user_guess = user_input()
+    user_guess = user_input()
 
-for i in range(len(weekdays)):
-    if user_guess==weekdays[i]:
-        print("Today is {}. On {}s, Captain Rainbow wears \n {}".format(user_guess, user_guess, outfits[user_guess]))
+    for i in range(len(weekdays)):
+        if user_guess==weekdays[i]:
+            print("Today is {}. On {}s, Captain Rainbow wears \n {}".format(user_guess, user_guess, outfits[user_guess]))
+    
+    running = True
+    while running:
+        selection = crud_user_input(
+        "\nPress C to add to list, Q to stop loop, R to Read from list, D to remove items and P to display list: ")
+        running = select(selection)
 
-def user_input(prompt):
-    user_input = input(prompt)
-    return user_input
-
-#instead of inputting C,Q,R,etc, the user will input the day of the week, which will then determine the output
-# Select function for getting input from terminal.
-# user_value = user_input("Please Enter a value:")
-# print(user_value)
-#
-# def test():
-#     running = True
-#     while running:
-#         selection = user_input(
-#         "Press C to add to list, Q to stop loop, R to Read from list, D to remove items and P to display list")
-#         running = select(selection)
-
+    # test()
 
 
 # Select function for getting input from terminal.
